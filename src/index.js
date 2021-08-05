dbf6c463b9cafc16f32afca99c83160dfb747f66
RailonA <railonacosta@gmail.com>
2021-08-05 18:30:26 -0400

    Fix eslint

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.scss';
-import App from './scripts/app';
+import App from './components/app';
 
 const Calculator = () => (
   <App />
