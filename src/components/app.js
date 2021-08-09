8b8fa246b22e968bd154d7efcfe54824cc9f5041
RailonA <railonacosta@gmail.com>
2021-08-09 12:19:12 -0400

    Import Calc to App

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,13 @@
 import ButtonPanel from "./buttonPanel";
 import Display from "./display";
 import React from "react";
+import calculate from "../logic/calculate";
 
 export default function App () {
 
+    calculate({"next": 0,
+        "operation": "+",
+        "total": 0});
     return (
         <>
 
