97422692870f9d65bb000705e133cdd6fe6ead12
RailonA <railonacosta@gmail.com>
2021-08-13 14:00:58 -0400

    Fix Style of colc

--- src/components/app.js
+++ src/components/app.js
@@ -25,7 +25,7 @@ const App = () => {
   };
 
   return (
-    <div>
+    <div className="mainAppDiv">
       <Display result={resultHander(myState.total, myState.next)} />
       <ButtonPanel clickHandler={handleClick} />
     </div>
--- src/pages/calculator.js
+++ src/pages/calculator.js
@@ -1,10 +1,13 @@
 import React from 'react';
-
 import { Box } from '@material-ui/core';
 import App from '../components/app';
 
 const Calculator = () => (
-  <Box width="700px" mt={5} m="auto"><App /></Box>
+  <Box width="700px" mt={5} m="auto">
+    <div className="appDiv">
+      <App />
+    </div>
+  </Box>
 );
 
 export default Calculator;
